bde898bb5e078062babc2ad4d965cab5cbce2b02
PavelSB <1987PavelS@mail.ru>
2023-02-13 01:31:52 +0300

    lesson 5 task 2 done

--- lesson5_task2.java
+++ lesson5_task2.java
@@ -16,58 +16,54 @@ import java.util.*;
 
 public class lesson5_task2 {
     public static void main(String[] args) {
-
         String str1 = "total";
         String str2 = "paper";
         char[] ch1 = str1.toCharArray();
         char[] ch2 = new char[str2.length()];
-        for (int i = 0; i<str2.length();i++){
-            ch2[i] =  str2.charAt(i);
+        for (int i = 0; i < str2.length(); i++) {
+            ch2[i] = str2.charAt(i);
         }
-        boolean flag= true;
+        boolean flag = true;
         Map<Character, Character> db1 = new HashMap<>();
         db1.putIfAbsent(ch1[0], ch2[0]);
-        for (int i  = 1; i<str1.length(); i++){
-            if (db1.containsKey(ch1[i]) && db1.get(ch1[i]).equals(ch2[i])){
+        for (int i = 1; i < str1.length(); i++) {
+            if (db1.containsKey(ch1[i]) && db1.get(ch1[i]).equals(ch2[i])) {
                 continue;
-            }
-            else if(!db1.containsKey(ch1[i]) && db1.containsValue(ch2[i])){
+            } else if (!db1.containsKey(ch1[i]) && db1.containsValue(ch2[i])) {
                 flag = false;
-            }
-            else if (!db1.containsKey(ch1[i])){
+            } else if (!db1.containsKey(ch1[i])) {
                 db1.put(ch1[i], ch2[i]);
-            }
-            else {
+            } else {
                 flag = false;
-            }         
+            }
         }
-        System.out.println(flag);        
+        System.out.println(flag);
     }
 }
 
 /* public class lesson5_task2 {
     public static void main(String[] args) {
-        String str1 ="paper";
-        String str2 ="title";
-        Boolean prov=true;
-        Map<Character, Character> dic=new HashMap<Character,Character>();
-        if (str1.length()==str2.length()){
+        String str1 = "paper";
+        String str2 = "title";
+        Boolean prov = true;
+        Map<Character, Character> dic = new HashMap<Character, Character>();
+        if (str1.length() == str2.length()) {
             for (int i = 0; i < str1.length(); i++) {
                 if (dic.containsKey(str1.charAt(i))) {
-                    if (dic.get(str1.charAt(i))!=str2.charAt(i)){
-                        prov=false;
+                    if (dic.get(str1.charAt(i)) != str2.charAt(i)) {
+                        prov = false;
                     }
-                }else{
-                    if (dic.containsValue(str2.charAt(i))){
-                        prov=false; 
+                } else {
+                    if (dic.containsValue(str2.charAt(i))) {
+                        prov = false;
                     }
                     dic.put(str1.charAt(i), str2.charAt(i));
                 }
-                }
+            }
         } else {
-            prov=false;
+            prov = false;
         }
         System.out.println(prov);
-         
+
     }
 } */
