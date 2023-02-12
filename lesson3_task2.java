8a7095e3818d3235dbf78eb80560dbb4c4709110
PavelSB <1987PavelS@mail.ru>
2023-02-13 00:02:51 +0300

    lesson 3 task 2 done

---
+++ lesson3_task2.java
@@ -0,0 +1,35 @@
+// Заполнить список названиями планет Солнечной системы в произвольном порядке с повторениями.
+// Вывести название каждой планеты и количество его повторений в списке.
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class lesson3_task2 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Земля");
+        list.add("Марс");
+        list.add("Юпитер");
+        list.add("Юпитер");
+        list.add("Венера");
+        list.add("Плутон");
+        list.add("Сатурн");
+        list.add("Земля");
+        list.add("Земля");
+        System.out.println(list);
+        list.sort(Comparator.naturalOrder());
+
+        int count = 1;
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i).equals(list.get(i + 1))) {
+                count++;
+            } else {
+                System.out.print(list.get(i));
+                System.out.println(" " + count);
+                count = 1;
+            }
+        }
+        System.out.print(list.get(list.size() - 1));
+        System.out.println(" " + count);
+    }
+}
